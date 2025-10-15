55c7117ff1eae8f68517b5993d09f5afc1094239
Danielcard99 <dc2651065@gmail.com>
2025-10-15 03:24:45 -0300

    config: update environment variables for SendGrid integration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:4173', 'https://abencoadoredes-2025.vercel.app'],
+    origin: ['http://localhost:5173', 'https://abencoadoredes-2025.vercel.app'],
   });
 
   app.useGlobalPipes(
