d26533d02514e9d97228d268bf49276d954aca4f
Danielcard99 <dc2651065@gmail.com>
2025-10-24 01:03:57 -0300

    feat: configure AOS animation library

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -9,6 +12,14 @@ import Contact from "./pages/Contact";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 const App: React.FC = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1500,
+      once: true,
+      offset: 200,
+    });
+  }, []);
+
   return (
     <BrowserRouter>
       <ScrollToTop />
